527412af97971901d919575ac2dd22c76d8a6d56
Mauer-Oats <maueroats@users.noreply.github.com>
2026-01-13 15:24:16 +0000

    add cache line

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -7,7 +7,7 @@ Xvfb $DISPLAY -screen 0 $RESOLUTION &
 fluxbox &
 
 # 3. Start VNC Server (No password for dev, mapped to display :0)
-x11vnc -display $DISPLAY -nopw -forever -quiet &
+x11vnc -display $DISPLAY  -ncache 10 -nopw -forever -quiet &
 
 # 4. Start noVNC bridge (Maps VNC to WebSockets for the browser)
 websockify --web /usr/share/novnc 6080 localhost:5900 &
@@ -16,5 +16,5 @@ echo "Display ready! Access at http://localhost:6080"
 
 # 5. Run the Python application using uv
 # This ensures dependencies are synced before running
-uv sync
+# uv sync
 #exec uv run python main.py
